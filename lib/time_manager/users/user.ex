69bdb6781766fe01fcddd99a1c46f87cf508ac9b
d4v1dh <dhalley@null.net>
2023-10-30 12:59:56 +0100

    hashed password on create and update

--- lib/time_manager/users/user.ex
+++ lib/time_manager/users/user.ex
@@ -18,5 +18,13 @@ defmodule TimeManager.Users.User do
     |> unique_constraint(:email)
     |> unique_constraint(:username)
     |> validate_format(:email, ~r/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/) 
+    |> put_password_hash()
   end
+
+  #check if changeset is valid
+  defp put_password_hash(%Ecto.Changeset{valid?: true, changes: %{ password: password }} = changeset) do
+    change(changeset, password: Bcrypt.hash_pwd_salt(password))
+  end
+
+  defp put_password_hash(changeset), do: changeset
 end
