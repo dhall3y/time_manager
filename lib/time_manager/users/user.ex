35b8ffe617aee34f7d71cb35072d721567553b6a
d4v1dh <dhalley@null.net>
2023-10-30 10:59:41 +0100

    non hashed password added to users

--- lib/time_manager/users/user.ex
+++ lib/time_manager/users/user.ex
@@ -5,6 +5,7 @@ defmodule TimeManager.Users.User do
   schema "users" do
     field :username, :string
     field :email, :string
+    field :password, :string
 
     timestamps(type: :utc_datetime)
   end
@@ -12,8 +13,8 @@ defmodule TimeManager.Users.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:username, :email])
-    |> validate_required([:username, :email])
+    |> cast(attrs, [:username, :email, :password])
+    |> validate_required([:username, :email, :password])
     |> unique_constraint(:email)
     |> unique_constraint(:username)
     |> validate_format(:email, ~r/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/) 
