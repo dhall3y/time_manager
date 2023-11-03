d76ad43615e3681b9818c20d73bac26eefc9ca4a
chasmyr <lestratgabin5886@gmail.com>
2023-11-03 18:37:58 +0100

    docker

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM elixir:1.14
+ 
+RUN apt-get update && \
+    apt-get install -y postgresql-client && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+ 
+WORKDIR /app
+ 
+COPY . .
+ 
+RUN mix local.hex --force
+RUN mix local.rebar --force
+ 
+RUN mix deps.get
+ 
+RUN mix compile
+ 
+EXPOSE 4000
+ 
+CMD ["mix", "phx.server"]
--- config/dev.exs
+++ config/dev.exs
@@ -4,8 +4,9 @@ import Config
 config :time_manager, TimeManager.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "localhost",
+  hostname: "db",
   database: "time_manager_dev",
+  port: "5432",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
@@ -19,7 +20,7 @@ config :time_manager, TimeManager.Repo,
 config :time_manager, TimeManagerWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {0, 0, 0, 0}, port: 4000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
