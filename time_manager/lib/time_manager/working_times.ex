256d26e02d5caeef55f5b0b6c5642d7438d84458
Baptiste Lemonnier <baptiste.lmn@gmail.com>
2023-11-08 10:59:42 +0100

    error management for workingtime

--- time_manager/lib/time_manager/working_times.ex
+++ time_manager/lib/time_manager/working_times.ex
@@ -2,150 +2,75 @@ defmodule TimeManager.WorkingTimes do
   @moduledoc """
   The WorkingTimes context.
   """
-
   import Ecto.Query, warn: false
   alias TimeManager.Repo
   alias TimeManager.Users
   alias TimeManager.WorkingTimes.WorkingTime
 
+  #------ INDEX ---------------------
   def list_user_workingtimes(id) do
     case Users.get_user(id) do
       nil ->
         {:error, :user_not_found}
       user ->
-        working_time =
-        WorkingTime
-        |> where(user_id: ^id)
-        |> Repo.all()
-        case working_time do
-          [] ->
-            {:error, :workingtime_not_found}
+        working_times =
+        WorkingTime |> where(user_id: ^id) |> Repo.all()
+        IO.inspect(working_times)
+        case working_times do
+          working_times ->
+            IO.inspect('Tableau non vide')
+            {:ok, working_times}
           _ ->
-            {:ok, working_time}
+            IO.inspect('tableau vide')
+            {:error, :workingtime_not_found}
         end
     end
   end
 
+  def list_workingtime, do Repo.all(WorkingTime)
 
-  # def list_user_workingtimes(id) do
-  #   user = Repo.get(User, id)
+  def get_working_time(id), do: Repo.get(WorkingTime, id)
 
-  #   if user do
-  #     WorkingTime
-  #     |> where(user_id: ^id)
-  #     |> Repo.all()
-  #     |> case do
-  #       [] -> {:error, :no_working_times}
-  #       working_times -> {:ok, working_times}
-  #     end
-  #   else
-  #     {:error, :user_not_found}
-  #   end
-  # end
-
-
-
-  @doc """
-  Returns the list of workingtimes.
-
-  ## Examples
-
-      iex> list_workingtimes()
-      [%WorkingTime{}, ...]
-
-  """
-  def list_workingtimes do
-    Repo.all(WorkingTime)
+  def get_by(id, startTime, endTime) do
+    query = from(w in WorkingTime, where: w.user_id == ^id and w.start >= ^startTime and w.end <= ^endTime)
+    Repo.all(query)
   end
 
-  @doc """
-  Gets a single working_time.
+  #  Repo.get_by(WorkingTime, user_id: id, start: startTime, end: endTime)
 
-  Raises `Ecto.NoResultsError` if the Working time does not exist.
+  def get_one(user_id, id) do
+    Repo.get_by(WorkingTime, user_id: user_id, id: id)
+  end
 
-  ## Examples
 
-      iex> get_working_time!(123)
-      %WorkingTime{}
+  #------ CREATE ---------------------
 
-      iex> get_working_time!(456)
-      ** (Ecto.NoResultsError)
-
-  """
-  def get_working_time!(id), do: Repo.get!(WorkingTime, id)
-
-  @doc """
-  Creates a working_time.
-
-  ## Examples
-
-      iex> create_working_time(%{field: value})
-      {:ok, %WorkingTime{}}
-
-      iex> create_working_time(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_working_time(id, attrs \\ %{}) do
-    %WorkingTime{user_id: id}
-    |> WorkingTime.changeset(attrs)
-    |> Repo.insert()
+    case Users.get_user(id) do
+      nil -> {:error, :user_not_found}
+      user ->
+        %WorkingTime{user_id: id}
+        |> WorkingTime.changeset(attrs)
+        |> Repo.insert()
+    end
   end
 
-  @doc """
-  Updates a working_time.
 
-  ## Examples
+  #------ UPDATE ------------------------------
 
-      iex> update_working_time(working_time, %{field: new_value})
-      {:ok, %WorkingTime{}}
-
-      iex> update_working_time(working_time, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def update_working_time(%WorkingTime{} = working_time, attrs) do
     working_time
     |> WorkingTime.changeset(attrs)
     |> Repo.update()
   end
 
-  @doc """
-  Deletes a working_time.
-
-  ## Examples
-
-      iex> delete_working_time(working_time)
-      {:ok, %WorkingTime{}}
-
-      iex> delete_working_time(working_time)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_working_time(%WorkingTime{} = working_time) do
-    Repo.delete(working_time)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking working_time changes.
-
-  ## Examples
-
-      iex> change_working_time(working_time)
-      %Ecto.Changeset{data: %WorkingTime{}}
-
-  """
   def change_working_time(%WorkingTime{} = working_time, attrs \\ %{}) do
     WorkingTime.changeset(working_time, attrs)
   end
 
-  def get_by(id, startTime, endTime) do
-      query = from(w in WorkingTime, where: w.user_id == ^id and w.start >= ^startTime and w.end <= ^endTime)
-      Repo.all(query)
-  end
-    #  Repo.get_by(WorkingTime, user_id: id, start: startTime, end: endTime)
 
-  def get_one(user_id, id) do
-    Repo.get_by(WorkingTime, user_id: user_id, id: id)
+  #------ DELETE ---------------------
+  def delete_working_time(%WorkingTime{} = working_time) do
+    Repo.delete(working_time)
   end
 end
--- time_manager/lib/time_manager_web/controllers/working_time_controller.ex
+++ time_manager/lib/time_manager_web/controllers/working_time_controller.ex
@@ -8,68 +8,62 @@ defmodule TimeManagerWeb.WorkingTimeController do
   action_fallback TimeManagerWeb.FallbackController
 
   def index(conn, %{"userID" => id, "start" => startTime, "end" => endTime}) do
-    time = WorkingTimes.get_by(id, startTime, endTime)
-    render(conn, :index, workingtimes: time)
-  end
-
-  #currently send an empty array if no working time might need to change this
-  def index(conn, %{"userID" => id}) do
     working_times = WorkingTimes.list_user_workingtimes(id)
+    IO.inspect(working_times)
     case working_times do
       {:error, :workingtime_not_found} ->
         conn |> put_status(:not_found) |> render(:error, message: "Workingtime not found")
       {:error, :user_not_found} ->
         conn |> put_status(:not_found) |> render(:error, message: "User not found")
       {:ok, working_times} ->
-        render(conn, :index, workingtimes: working_times)
+        time = WorkingTimes.get_by(id, startTime, endTime)
+        render(conn, :index, workingtimes: time)
     end
   end
-
 
   def show(conn, %{"userID" => user_id, "id" => id}) do
     workingtime = WorkingTimes.get_one(user_id, id)
     render(conn, :index, workingtimes: workingtime)
   end
 
-  # def show(conn, %{"userID" => user_id, "id" => id}) do
-  #   case WorkingTimes.get_one(user_id, id) do
-  #     %WorkingTime{} = workingtime ->
-  #       conn
-  #       |> put_status(:ok)
-  #       |> render(:index, workingtimes: workingtime)
-  #     _ ->
-  #       conn
-  #       |> put_status(:bad_request)
-  #       |> render(:error, message: "couldn't find workingtime")
-  #   end
-  # end
-
   def create(conn, %{"userID" => id, "start" => startTime, "end" => endTime}) do
     id = String.to_integer(id)
     working_time_params = %{start: startTime, end: endTime}
+    workingtimes_create = WorkingTimes.create_working_time(id, working_time_params)
 
-    with {:ok, %WorkingTime{} = working_time} <- WorkingTimes.create_working_time(id, working_time_params) do
-      conn
-      |> put_status(:created)
-      |> put_resp_header("location", ~p"/api/workingtimes/#{working_time}")
-      |> render(:index, workingtimes: working_time)
-    end
+    case workingtimes_create do
+      {:error, :user_not_found} ->
+        conn |> put_status(:not_found) |> render("error.json", message: "User not found")
+      {:error, %Ecto.Changeset{} = changeset} ->
+        conn |> put_status(:unprocessable_entity) |> render("error.json", message: "Invalid format")
+      {:ok, working_time} ->
+        conn |> put_status(:created) |> put_resp_header("location", ~p"/api/workingtimes/#{working_time}") |> render("show.json", working_time: working_time)
+      end
   end
 
   def update(conn, %{"id" => id, "start" => startTime, "end" => endTime}) do
-    working_time = WorkingTimes.get_working_time!(id)
-    working_time_params = %{start: startTime, end: endTime}
-
-    with {:ok, %WorkingTime{} = working_time} <- WorkingTimes.update_working_time(working_time, working_time_params) do
-      render(conn, :index, workingtimes: working_time)
+    working_time = WorkingTimes.get_working_time(id)
+    case working_time do
+      nil ->
+        conn |> put_status(:not_found) |> render("error.json", message: "Workingtime not found")
+      {:ok, working_time} ->
+        working_time_params = %{start: startTime, end: endTime}
+        with {:ok, %WorkingTime{} = working_time} <- WorkingTimes.update_working_time(working_time, working_time_params) do
+          render(conn, :index, workingtimes: working_time)
+        end
     end
   end
 
   def delete(conn, %{"id" => id}) do
-    working_time = WorkingTimes.get_working_time!(id)
-
-    with {:ok, %WorkingTime{}} <- WorkingTimes.delete_working_time(working_time) do
-      send_resp(conn, :no_content, "")
+    working_time = WorkingTimes.get_working_time(id)
+    case working_time do
+      nil ->
+        conn |> put_status(:not_found) |> render("error.json", message: "Workingtime not found")
+      _ ->
+        IO.inspect('test -------------------------------------')
+        with {:ok, %WorkingTime{}} <- WorkingTimes.delete_working_time(working_time) do
+          send_resp(conn, :no_content, "")
+        end
     end
   end
 end
