571f4826bb6e7cb7593bf0c749d77e131d2c0407
Baptiste Lemonnier <baptiste.lmn@gmail.com>
2023-11-10 15:00:17 +0100

    change dev to prod

--- time_manager_app/src/utils/api.js
+++ time_manager_app/src/utils/api.js
@@ -1,25 +1,25 @@
 import axios from "axios"
 
 export const ApiGet = async (url) => {
-    return await axios.get(`http://35.180.138.117:4000/api${url}`)
+    return await axios.get(`localhost:4000/api${url}`)
     .then(res => res.data)
     .catch(err => console.log(err))
 }
 
 export const ApiPost = async (url, body) => {
-    return await axios.post(`http://35.180.138.117:4000/api${url}`, body)
+    return await axios.post(`localhost:4000/api${url}`, body)
     .then(res => { return res })
     .catch(err => console.log(err))
 }
 
 export const ApiDelete = async (url) => {
-    return await axios.delete(`http://35.180.138.117:4000/api${url}`)
+    return await axios.delete(`localhost:4000/api${url}`)
     .then(res => res.data)
     .catch(err => console.log(err))
 }
 
 export const ApiPut = async (url, body) => {
-    return await axios.put(`http://35.180.138.117:4000/api${url}`, body)
+    return await axios.put(`localhost:4000/api${url}`, body)
     .then(res => res.data)
     .catch(err => console.log(err))
 }
