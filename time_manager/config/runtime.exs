1ebdd7067961676e2416022a030c3cf7f84b5d86
Baptiste Lemonnier <baptiste.lmn@gmail.com>
2023-11-10 16:12:07 +0100

    change config

--- time_manager/config/runtime.exs
+++ time_manager/config/runtime.exs
@@ -34,7 +34,9 @@ if config_env() == :prod do
     # ssl: true,
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "20"),
-    socket_options: maybe_ipv6
+    socket_options: maybe_ipv6,
+    database: database_url
+
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
