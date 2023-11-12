a0f7a7b82a5dcb022424b2030d850c251c3431a5
Chasmyr <78807306+Chasmyr@users.noreply.github.com>
2023-11-12 19:21:51 +0100

    login fix

--- time_manager_app/src/views/Login.vue
+++ time_manager_app/src/views/Login.vue
@@ -21,12 +21,12 @@ export default {
             if(this.email.length > 0 && this.password.length > 0) {
                 this.isClickable = false
 
-                const password = this.password;
-                const encoder = new TextEncoder()
-                const data = encoder.encode(password)
-                const hash = await crypto.subtle.digest("SHA-256", data)
-                const hashArray = Array.from(new Uint8Array(hash))
-                const hashHex = hashArray.map((b) => b.toString(16).padStart(2, "0")).join("")
+                // const password = this.password;
+                // const encoder = new TextEncoder()
+                // const data = encoder.encode(password)
+                // const hash = await crypto.subtle.digest("SHA-256", data)
+                // const hashArray = Array.from(new Uint8Array(hash))
+                // const hashHex = hashArray.map((b) => b.toString(16).padStart(2, "0")).join("")
 
                 let body = {
                     email: this.email,
@@ -57,12 +57,12 @@ export default {
             if(this.email.length > 0 && this.password.length > 0 && this.username.length > 0) {
                 this.isClickable = false
 
-                const password = this.password;
-                const encoder = new TextEncoder()
-                const data = encoder.encode(password)
-                const hash = await crypto.subtle.digest("SHA-256", data)
-                const hashArray = Array.from(new Uint8Array(hash))
-                const hashHex = hashArray.map((b) => b.toString(16).padStart(2, "0")).join("")
+                // const password = this.password;
+                // const encoder = new TextEncoder()
+                // const data = encoder.encode(password)
+                // const hash = await crypto.subtle.digest("SHA-256", data)
+                // const hashArray = Array.from(new Uint8Array(hash))
+                // const hashHex = hashArray.map((b) => b.toString(16).padStart(2, "0")).join("")
 
                 let body = {
                     email: this.email,
